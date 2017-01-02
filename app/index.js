c0bb54e551ea4edb2842994df6ed5d9705180192
Chris McNeese <christophermcneese@mac.com>
2017-01-02 11:32:02 -0600

    Generated Broccoli Browserify Project

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import 'whatwg-fetch';
-const apiKey = 5e2916234effe6286328f704c9ad7f40;
+const apiKey = '5e2916234effe6286328f704c9ad7f40';
 import http://gateway.marvel.com:80/v1/public/series/9856?apikey=5e2916234effe6286328f704c9ad7f40
 
 const app = new Vue({
